38293f894e0006f6005ea9c1f8f3a8f9eb75e61f
Polaris106 <wangxingrui2134@gmail.com>
2023-03-14 23:35:28 +0800

    Login and Registration ver1.3
    ->removed title at the top of activity_main page
    ->changed app name to SALT

--- app/src/main/java/com/example/authapp/HomePage.java
+++ app/src/main/java/com/example/authapp/HomePage.java
@@ -2,19 +2,29 @@ package com.example.authapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity implements View.OnClickListener{
+    private TextView banner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        switch(view.getId()){
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
     }
 }
